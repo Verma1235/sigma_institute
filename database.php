711f9ad0616e131df2a3aea3e0758e6dbac548e2
Dinesh verma <vermadinesh9693@gmail.com>
2026-03-07 16:55:55 +0530

    update db connection

--- database.php
+++ database.php
@@ -1,24 +1,31 @@
 <?php
 
+$host = getenv("DB_HOST") ?: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$user = getenv("DB_USER") ?: "sigma";
+$pass = getenv("DB_PASS") ?: "YOUR_PASSWORD";
+$db   = getenv("DB_NAME") ?: "YOUR_DATABASE";
+$port = getenv("DB_PORT") ?: 4000;
 
+$conn = mysqli_init();
 
-$host = getenv("DB_HOST") ?: "localhost";
-$user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS") ?: "";
-$db = getenv("DB_NAME") ?: "sigma";
-$port = getenv("DB_PORT") ?: 3306;
+/* Enable SSL (required by TiDB Cloud) */
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 }
 
-// if($conn){
-//     echo "Connected";
-// }
-// else{
-//     echo"Error".mysqli_connect_error();
-// }
+// echo "Connected successfully";
 
 ?>
